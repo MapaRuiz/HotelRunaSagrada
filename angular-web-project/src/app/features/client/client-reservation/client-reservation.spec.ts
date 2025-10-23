2dd86d9d4eea2a4f7ab2b389c69bc23e3886c778
SDM30 <simondiaz@javeriana.edu.co>
2025-10-23 13:29:23 -0500

    factura

---
+++ angular-web-project/src/app/features/client/client-reservation/client-reservation.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ClientReservation } from './client-reservation';
+
+describe('ClientReservation', () => {
+  let component: ClientReservation;
+  let fixture: ComponentFixture<ClientReservation>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ClientReservation]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ClientReservation);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ angular-web-project/src/app/features/client/client-reservation/client-reservation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-reservation',
+  imports: [],
+  templateUrl: './client-reservation.html',
+  styleUrl: './client-reservation.css'
+})
+export class ClientReservation {
+
+}
